e2f1b0d1ef4085754beb3d776231583f579082b2
Nic Harvey <nharvey@bscglobal.com>
2023-02-13 11:54:36 +0200

    remove unnecessary tests

--- test/unit/test_ops.py
+++ test/unit/test_ops.py
@@ -50,34 +50,3 @@ def test_unchunk():
     result = x.chunk(n_chunks=3).unchunk()
 
     assert result.collect() == [1, 2, 3, 4, 5, 6]
-
-
-def test_map_when_multiple_collect():
-    x = jabl(1, 2, 3, 4, 5, 6)
-    y = jabl("A", "B", "C", "D", "E", "F")
-    when: Callable[[int], bool] = lambda x: x % 2 == 0
-
-    result = x.map_when(y, when)
-
-    first_collect = result.collect()
-    next_collect = result.collect()
-
-    assert first_collect == next_collect
-    
-
-def test_map_when_explicit():
-    x = jabl(1, 2, 3, 4, 5, 6)
-    predicate = jabl(True, False, True, False, True, True)
-
-    result = x.map_when(lambda x: x + 1, predicate)
-
-    assert result.collect() == [2, 2, 4, 4, 6, 7]
-
-
-def test_map_when_lazy():
-    x = jabl(1, 2, 3, 4, 5, 6)
-    predicate: Callable[[int], bool] = lambda x: x % 2 == 0
-
-    result = x.map_when(lambda x: x + 1, predicate)
-
-    assert result.collect() == [1, 3, 3, 5, 5, 7]
